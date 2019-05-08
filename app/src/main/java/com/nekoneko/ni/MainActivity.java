109ddcc9944e7eaf115501474d20c85a77541475
izen1231 <izen1231@gmail.com>
2019-05-09 07:59:32 +0900

    fragment update

--- app/src/main/java/com/nekoneko/ni/MainActivity.java
+++ app/src/main/java/com/nekoneko/ni/MainActivity.java
@@ -1,90 +1,58 @@
 package com.nekoneko.ni;
 
-import android.Manifest;
-import android.app.FragmentManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.Button;
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.view.View;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
 
-    private RecyclerView mNekoRecyclerView;
-    private RecyclerView.Adapter mNekoAdapter;
-    private RecyclerView.LayoutManager mNekoLayoutManger;
+    private FragmentOne fragmentone;
+    private FragmentTwo fragmenttwo;
 
-    private Button mAddNekoButton;
-
-    public static final int REQUEST_CODE_MAIN = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //뷰 정의하기
-        mNekoRecyclerView = findViewById(R.id.mNekoRecyclerView);
-        mAddNekoButton = findViewById(R.id.mAddNekoButton);
+        fragmentone = new FragmentOne();
+        fragmenttwo = new FragmentTwo();
+        initFragment();
 
-        //mNekoLayoutManger 설정하기
-        mNekoLayoutManger = new LinearLayoutManager(getApplicationContext());
-        mNekoRecyclerView.setLayoutManager(mNekoLayoutManger);
 
-        //mNekoAdapter 설정하기
-        mNekoAdapter = new NekoAdapter();
-        mNekoRecyclerView.setAdapter(mNekoAdapter);
-
-        //버튼 리스너 등록하기
-        mAddNekoButton.setOnClickListener(new android.view.View.OnClickListener() {
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
-            public void onClick(android.view.View v) {
-                Log.d(TAG, "Button Clicked");
+            public void onTabSelected(@IdRes int tabId) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-                //Location Manager 객체 가져오기
-                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-                //위치 퍼미션 체크하기
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    return;
+                if(tabId == R.id.tab_home){
+                    transaction.replace(R.id.contentContainer, fragmentone).commit();
                 }
 
-                Log.d(TAG,"MapActivity");
+                else if(tabId == R.id.tab_star) {
+                    transaction.replace(R.id.contentContainer, fragmenttwo).commit();
 
-                //가장 최근의 위치정보 가져오기
-                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                }
 
-                //인텐트 준비하기
-                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                }
 
-                intent.putExtra("longitude", location.getLongitude());  //위도 가져오기
-                intent.putExtra("latitude", location.getLatitude());    //경도 가져오기
-
-                //MapActivity로 화면 전환하기
-                startActivityForResult(intent,REQUEST_CODE_MAIN);
-            }
         });
+}
+
+
+    /*
+     App 실행시 보여지는 Fragment 설정.
+    */
+    public void initFragment(){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.contentContainer, fragmentone);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
+
 }
