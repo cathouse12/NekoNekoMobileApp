999992587ef75b7715c82dd061bf2b4ba5e6d077
phs1234 <32615067+phs1234@users.noreply.github.com>
2019-05-08 23:41:32 +0900

    do nothing

--- app/src/main/java/com/nekoneko/ni/MainActivity.java
+++ app/src/main/java/com/nekoneko/ni/MainActivity.java
@@ -1,12 +1,19 @@
 package com.nekoneko.ni;
 
+import android.Manifest;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,23 +35,55 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mNekoAdapter;
     private RecyclerView.LayoutManager mNekoLayoutManger;
 
+    private Button mAddNekoButton;
+
+    public static final int REQUEST_CODE_MAIN = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //뷰 정의하기
         mNekoRecyclerView = findViewById(R.id.mNekoRecyclerView);
+        mAddNekoButton = findViewById(R.id.mAddNekoButton);
 
+        //mNekoLayoutManger 설정하기
         mNekoLayoutManger = new LinearLayoutManager(getApplicationContext());
         mNekoRecyclerView.setLayoutManager(mNekoLayoutManger);
 
+        //mNekoAdapter 설정하기
         mNekoAdapter = new NekoAdapter();
-
         mNekoRecyclerView.setAdapter(mNekoAdapter);
-    }
 
-    public void addButtonClicked(View view){
-        Intent intent = new Intent(getApplicationContext(),MapActivity.class);
-        startActivity(intent,g);
+        //버튼 리스너 등록하기
+        mAddNekoButton.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                Log.d(TAG, "Button Clicked");
+
+                //Location Manager 객체 가져오기
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                //위치 퍼미션 체크하기
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.d(TAG,"MapActivity");
+
+                    //가장 최근의 위치정보 가져오기
+                    Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    //인텐트 준비하기
+                    Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+
+                    intent.putExtra("longitude", location.getLongitude());  //위도 가져오기
+                    intent.putExtra("latitude", location.getLatitude());    //경도 가져오기
+
+                    //MapActivity로 화면 전환하기
+                    startActivityForResult(intent,REQUEST_CODE_MAIN);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/nekoneko/ni/MapActivity.java
+++ app/src/main/java/com/nekoneko/ni/MapActivity.java
@@ -2,7 +2,6 @@ package com.nekoneko.ni;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,13 +9,19 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private double mInitialLongitude;
+    private double mInitialLatitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //처음 줌 인(Zoom In)할 위도와 경도를 가져온다
+        mInitialLongitude = getIntent().getDoubleExtra("longitude",0);
+        mInitialLatitude = getIntent().getDoubleExtra("latitude", 0);
 
         //프래그먼트 세팅하기
         FragmentManager fragmentManager = getFragmentManager();
@@ -46,16 +51,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng SEOUL = new LatLng(37.56,126.97);
+        //처음 줌 인(Zoom in)할 위도와 경도 설정하기
+        LatLng SEOUL = new LatLng(mInitialLongitude,mInitialLatitude);
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-
-        googleMap.addMarker(markerOptions);
-
+        //처음 설정한 위치 좌표로 카메라 이동하기
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(100));
+
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(SEOUL);
+//        markerOptions.title("서울");
+//        markerOptions.snippet("한국의 수도");
+//
+//        googleMap.addMarker(markerOptions);
     }
 }
